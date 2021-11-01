31d11797d1bd5b03b34cd07f19a6c7befe66d955
Eshaik <eduin.shaik.kommit@gmail.com>
2021-10-31 20:53:17 -0500

    Add username to User, now can login with username or email.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@
 # Application record class: Class to the model of MVC, to create and use datas.
 # Full guide: https://guides.rubyonrails.org/active_record_basics.html
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  added_attrs
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[username email password password_confirmation remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,8 @@
 
 # Article controller: Controller to set all the methods and relations about Article.
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: 'Eduin', password: 'secret', except: %i[index show]
-
+  #http_basic_authenticate_with name: 'Eduin', password: 'secret', except: %i[index show]
+  before_action :authenticate_user!, except: %i[index show]
   def index
     @articles = Article.all
   end
