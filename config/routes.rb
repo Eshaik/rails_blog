4d7d38e06e8dbce3c8160ef66f8de8542ea2e343
Eshaik <eduin.shaik.kommit@gmail.com>
2021-11-01 14:48:12 -0500

    Add styles to log in/sign up/forget password, add messages when log in is wrong, add author to article table, create/edit/delete my blog posts, add comments in other blog posts only for logged users.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root 'articles#index'
   get '/notfound', to: "others#notfound"
 
+  #devise_scope :user do
+  #  get '/signout', to: 'devise/sessions#destroy', as: :signout
+  #end
+
   resources :articles do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_223419) do
+ActiveRecord::Schema.define(version: 2021_11_01_154457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_223419) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_223419) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "info", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
